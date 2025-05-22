73272c52e1c2f2c4c51dcb86666cf4ae059b7ca4
thetschulian <140388634+thetschulian@users.noreply.github.com>
2025-05-22 09:19:04 +0200

    Create vaultwarden-ubuntu-commands.sh

---
+++ vaultwarden-ubuntu-commands.sh
@@ -0,0 +1,26 @@
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+ 
+ 
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ 
+sudo apt-get update -y
+ 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker vault
+ 
+mkdir Vault
+mkdir Vault/certs
+
+sudo openssl req -x509 -nodes -days 3650 -newkey rsa:2048   -keyout Vault/certs/vaultwarden.key   -out Vault/certs/vaultwarden.crt   -subj "/CN=vaultwarden.local"
+
+touch Vault/docker-compose.yaml
+	 
+sudo  chown -R un26-vault /home/vault
+sudo reboot
