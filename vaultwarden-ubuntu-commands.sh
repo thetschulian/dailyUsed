651241ea942db5ff7a29a5c87a007364f845df12
thetschulian <140388634+thetschulian@users.noreply.github.com>
2025-07-03 08:52:30 +0200

    Update vaultwarden-ubuntu-commands.sh

--- vaultwarden-ubuntu-commands.sh
+++ vaultwarden-ubuntu-commands.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl nfs-common
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
