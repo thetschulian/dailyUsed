cc17615bac29c867a7b122964567ccb36f0e271a
thetschulian <140388634+thetschulian@users.noreply.github.com>
2025-09-10 15:43:03 +0200

    Update lenovo_x_unix_battery.sh

--- useless_junk/lenovo_x_unix_battery.sh
+++ useless_junk/lenovo_x_unix_battery.sh
@@ -1,4 +1,7 @@
 echo ##### Install Lenovo ThinkPad Power Manager on UNIX
+echo # Useful for Laptops used as a Lab or Home-Server
+echo ######################################################
+
 sudo apt update -y
 sudo apt install tlp tlp-rdw -y
 
@@ -10,22 +13,20 @@ echo ##### Checking if Battery is supported / found
 sudo tlp-stat 
 
 echo ##### Editing the Config /etc/tlp.conf
-echo Uncomment this Line: START_CHARGE_THRESH_BAT0=80 
-echo Uncomment this Line: STOP_CHARGE_THRESH_BAT0=85
+echo # Uncomment this Line: START_CHARGE_THRESH_BAT0=80 
+echo # Uncomment this Line: STOP_CHARGE_THRESH_BAT0=85
 
 sudo nano /etc/tlp.conf
-
 sudo systemctl restart tlp
 
 echo ##### Check Battery-Level
 /etc/update-motd.d/10-battery
 
 echo ##### Disable Hibernate/Sleep/Standby by closing LID
-echo Uncomment this Line: HandleLidSwitch=ignore
-echo Uncomment this Line: HandleLidSwitchDocked=ignore
-echo Uncomment this Line: HandleLidSwitchExternalPower=ignore
+echo # Uncomment this Line: HandleLidSwitch=ignore
+echo # Uncomment this Line: HandleLidSwitchDocked=ignore
+echo # Uncomment this Line: HandleLidSwitchExternalPower=ignore
 
 sudo nano /etc/systemd/logind.conf
-
 sudo systemctl restart systemd-logind
 
