0d0986fe542e411fb622bd5fb3587ad82ee5792b
thetschulian <140388634+thetschulian@users.noreply.github.com>
2025-09-10 15:18:45 +0200

    Update lenovo_x_unix_battery.sh

--- useless_junk/lenovo_x_unix_battery.sh
+++ useless_junk/lenovo_x_unix_battery.sh
@@ -1,13 +1,23 @@
-echo Install Lenovo Think Pad Power Manager on UNIX
+echo ##### Install Lenovo Think Pad Power Manager on UNIX
 sudo apt update -y
 sudo apt install tlp tlp-rdw -y
 
 sudo systemctl enable tlp --now
 sudo systemctl start tlp
 
+
+
+
+echo ##### Checking if Battery is supported / found
+sudo tlp-stat 
+
+echo ##### Editing the Config /etc/tlp.conf
+echo Uncomment this Line: START_CHARGE_THRESH_BAT0=80 
+echo Uncomment this Line: STOP_CHARGE_THRESH_BAT0=85
+
 sudo nano /etc/tlp.conf
 
 sudo systemctl restart tlp
 
-echo Check Battery-Level
+echo ##### Check Battery-Level
 /etc/update-motd.d/10-battery
