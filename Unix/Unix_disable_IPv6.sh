ab5b8554215a95ba4b5dcb122aaaef88f290f598
thetschulian <140388634+thetschulian@users.noreply.github.com>
2025-09-15 14:30:09 +0200

    Update Unix_disable_IPv6.sh

--- Unix/Unix_disable_IPv6.sh
+++ Unix/Unix_disable_IPv6.sh
@@ -5,3 +5,14 @@ echo the line should look like this
 echo GRUB_CMDLINE_LINUX_DEFAULT="quiet ipv6.disable=1"
 
 sudo update-grub
+
+
+echo debian
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
+echo ubuntu
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
