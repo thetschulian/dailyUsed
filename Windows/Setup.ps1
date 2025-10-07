f8c6de3af5cb33f2f6700c6a45e7cd8228f4d2aa
thetschulian <140388634+thetschulian@users.noreply.github.com>
2025-10-07 11:09:22 +0200

    Update Setup.ps1

--- Windows/Setup.ps1
+++ Windows/Setup.ps1
@@ -3,4 +3,4 @@
   curl -o "Windows-Usr-Basics.cmd" "https://raw.githubusercontent.com/thetschulian/dailyUsed/refs/heads/main/Windows/Windows-Usr-Basics.cmd"
 
 # Bypass Win11 requirements for installing on "old" Hardware
-  curl -o "Windows11_install_bypass.reg" "https://raw.githubusercontent.com/thetschulian/dailyUsed/main/Windows11_install_bypass.reg"
+  curl -o "Windows11_install_bypass.reg" "https://raw.githubusercontent.com/thetschulian/dailyUsed/refs/heads/main/Windows/Windows11_install_bypass.reg"
