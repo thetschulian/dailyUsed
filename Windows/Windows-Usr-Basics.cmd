f9960fff8d011bdef87b7ac5091c2c8365a46ff5
thetschulian <140388634+thetschulian@users.noreply.github.com>
2026-02-04 11:01:31 +0100

    Update Windows-Usr-Basics.cmd

--- Windows/Windows-Usr-Basics.cmd
+++ Windows/Windows-Usr-Basics.cmd
@@ -264,6 +264,9 @@ set "adminScript=%basicTempDir%\Windows-Admin-Tasks.cmd"
     echo @echo off > "%adminScript%"
     echo echo Running elevated tasks... >> "%adminScript%"
 
+	echo Enable PING requests
+	echo netsh advfirewall firewall add rule name="Allow ICMPv4-In" protocol=icmpv4:8,any dir=in action=allow >> "%adminScript%"
+
 
 	echo schtasks /create /tn "daily_Kill_Tool" /tr "C:\Windows\System32\taskkill.exe /IM Tool.exe /F" /sc daily /st 20:00 /ru "SYSTEM" /f >> "%adminScript%"
 
